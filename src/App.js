bc8c13cd5cc197aafb4b80ad51717c9872c96394
viralgupta <guptaviral0@gmail.com>
2023-10-21 21:03:01 +0530

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import LeftCanvas from "./Components/leftCanvas";
 import Background from "./Components/background";
 import Footer from "./Components/Footer";
 import RightCanvas from "./Components/RightCanvas";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
       <LeftCanvas/>
       <RightCanvas/>
       <Footer />
+      <Analytics/>
     </div>
   );
 }
