35b5b424be300fcbd0b598de5bf46fb4df5e3199
viralgupta <guptaviral0@gmail.com>
2023-11-07 01:17:28 +0530

    url change footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
   let year = date.getFullYear();
   return (
     <div className="flex flex-row items-center p-1 bg-opacity-50 bg-black z-50 absolute w-full justify-between px-2">
-      <a href="https://github.com/soumyajit4419/Portfolio" target="_blank" rel="noreferrer" className="text-white font-large items-center hidden md:flex">Inspiration from Soumyajit &nbsp;<BiLinkExternal/></a>
+      <a href="https://soumyajit.vercel.app/" target="_blank" rel="noreferrer" className="text-white font-large items-center hidden md:flex">Inspiration from Soumyajit &nbsp;<BiLinkExternal/></a>
       <div className="text-white">
         Copyright&nbsp;©&nbsp;{year}&nbsp;VG
       </div>
