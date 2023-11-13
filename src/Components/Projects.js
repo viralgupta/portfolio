2f3ceed813a6f72a73fb01356fcdf598de69db09
viralgupta <guptaviral0@gmail.com>
2023-11-13 11:13:54 +0530

    updated projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
                 <strong className="text-green-100">Projects </strong> I have made
             </div>
             <div className="flex flex-wrap md:px-5">
-                <ProjectCard Title={"DigiBooking"} Tags={"Webdev IOT"} Description={"Event booking website, Using face data to book images, Your Face will be your Tickets! Made using AWS Rekognition."} Link={"https://github.com/viralgupta/digi-booking"} Imagelink={"Assets/Projects/digibooking.png"} />
+                <ProjectCard Title={"DigiBooking"} Tags={"Webdev IOT"} Description={"Event booking website, Using face data to book images, Your Face will be your Tickets! Made using AWS Rekognition."} Link={"https://github.com/viralgupta?tab=repositories&q=digi-booking"} Imagelink={"Assets/Projects/digibooking.png"} />
                 <ProjectCard Title={"Real Time Chess Website"} Tags={"Webdev"} Description={"Chess Website where you can search people, accept/reject requests to play, Made using Socket.io"} Link={"https://github.com/viralgupta/chess"} Imagelink={"Assets/Projects/chess.png"} />
                 <ProjectCard Title={"Youtube Subscription Only"} Tags={"Webdev"} Description={"Simple website to get your Youtube Subscription Feed, to help you get over distractions, Implemented using OAuth2.0"} Link={"https://youtube-subscription-only.vercel.app/"} Imagelink={"Assets/Projects/yso.png"} />
                 <ProjectCard Title={"Whatsapp Attendance Bot"} Tags={"WebDev"} Description={`Whatsapp bot to continously check attendance on your behalf and notify you when attendance has reduced! (Banned)`} Link={"https://github.com/viralgupta/Whatsapp-Attendance-Bot"} Imagelink={"/Assets/Projects/whatsappbot.png"} />
