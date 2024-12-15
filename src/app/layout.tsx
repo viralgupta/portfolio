3d3e6fb36b78c5a534a573e84d2ce4cbeb923e14
Amrit <iamamrit27@gmail.com>
2024-12-15 11:46:05 +0530

    default theme to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider attribute="class" defaultTheme="dark">
           <TooltipProvider delayDuration={0}>
             {children}
             <Navbar />
