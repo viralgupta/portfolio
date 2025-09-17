5256bb3a1d170d4385100eb9757938a9ebdbd6be
amrit <iamamrit27@gmail.com>
2025-09-17 21:46:26 +0530

    remove dead code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { HackathonCard } from "@/components/hackathon-card";
 import BlurFade from "@/components/magicui/blur-fade";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
-import { ProjectCard } from "@/components/project-card";
 import { ResumeCard } from "@/components/resume-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
